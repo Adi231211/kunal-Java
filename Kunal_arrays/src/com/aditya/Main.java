dcf96d7c9281d89b5ae7ce38b90701d979f81e4c
Aditya Vikram Singh <adityavs2003@gmail.com>
2025-01-25 14:38:56 +0530

    file updated

--- Kunal_arrays/src/com/aditya/Main.java
+++ Kunal_arrays/src/com/aditya/Main.java
@@ -1,7 +1,7 @@
 package com.aditya;
 
-public class hello {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("hello");
     }
 }
---
+++ Kunal_arrays/src/com/sarita/study.java
@@ -0,0 +1,7 @@
+package com.sarita;
+
+public class study {
+    public static void main(String[] args) {
+        System.out.println("yeah");
+    }
+}
